48d956be51c1251fc8cc7e611882faa9d46322ec
Paul M. Jones <pmjones88@gmail.com>
2015-03-03 09:02:21 -0600

    change styling

--- templates/navfooter.php
+++ templates/navfooter.php
@@ -5,26 +5,26 @@ $next = $this->page->getNext();
 ?>
 
 <nav class="navfooter">
-    <table width="100%">
+    <table>
         <tr>
-            <td width="40%" align="left"><?php if ($prev) {
+            <td width="40%" class="prev"><?php if ($prev) {
                 echo $this->anchorRaw($prev->getHref(), 'Prev');
             } ?></td>
-            <td width="20%" align="center"><?php if ($parent) {
+            <td width="20%" class="parent"><?php if ($parent) {
                 echo $this->anchorRaw($parent->getHref(), 'Up');
             } ?></td>
-            <td width="40%" align="right"><?php if ($next) {
+            <td width="40%" class="next"><?php if ($next) {
                 echo $this->anchorRaw($next->getHref(), 'Next');
             } ?></td>
         </tr>
         <tr>
-            <td width="40%" align="left" valign="top"><?php if ($prev) {
+            <td width="40%" class="prev"><?php if ($prev) {
                 echo $prev->getNumberAndTitle();
             } ?></td>
-            <td width="20%" align="center" valign="top"><?php if ($parent) {
+            <td width="20%" class="parent"><?php if ($parent) {
                 echo $parent->getNumberAndTitle();
             } ?></td>
-            <td width="40%" align="right" valign="top"><?php if ($next) {
+            <td width="40%" class="next"><?php if ($next) {
                 echo $next->getNumberAndTitle();
             } ?></td>
         </tr>
